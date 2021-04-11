55de61d9be8192051a2c91f16208f001508dbe1a
SkyBlueFlash <CyperFlash@protonmail.com>
2021-04-11 23:10:10 +0300

    added a class contructer for Snake and changed list<segment> body to public

--- player.h
+++ player.h
@@ -7,12 +7,18 @@ using namespace std;
 
 // a segment of the snake's body
 struct Segment {
-    int state; // alive or dead (1 or 0)
-} segment;
+    bool state; // alive or dead (1 or 0)
+};
 
 class Snake {
+public:
+    Snake() {
+        Segment s1;
+        s1.state = false;
+        body.push_back(s1);
+    }
+    list<Segment> body; // the snake's body
 private:
-    list<struct Segment> body; // the snake's body
     void move();
     void change_direc();
 };
